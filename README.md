737cd0a083e7775115fef75a2f44dfe9e2ad267f
Andrew Glover <ajglover@gmail.com>
2011-12-19 17:02:58 -0500

    updated docs and have bitly working

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ convert HTML into Jade, see [Github project](https://github.com/donpark/html2jad
 ../../node_modules/html2jade/bin/html2jade test_upload.html 
 ```
 
-Heroku app, lives at [http://antalya.herokuapp.com/index](http://antalya.herokuapp.com/index); thus, Heroku app is antalya
+Heroku app, lives at [http://antalya.herokuapp.com/index](http://antalya.herokuapp.com/index); thus, Heroku app is antalya.
+
+Libraries used:
+
+* [Knox](https://github.com/LearnBoost/knox)
+* [Bitly](https://github.com/tanepiper/node-bitly)
+
+
+Good reference for uploading stuff: [uploader](https://github.com/bradleyg/uploader)
--- web.js
+++ web.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var form = require('connect-form');
+var config = require('./config/config.js');
+var knox = require('knox');
+var Bitly = require('bitly');
+var urlshort = new Bitly('aglover', 'R_b47321c6fbe1cd3b3f951e3eaaaf49d1');
+
+var kclient = knox.createClient({
+    key: config.s3.key, 
+    secret: config.s3.secret, 
+    bucket: config.s3.bucket
+});
 
 var app = express.createServer(form({ keepExtensions: true }));
 app.set('view engine', 'jade');
@@ -25,9 +35,27 @@ app.post('/', function(req, res, next){
     if (err) {
       next(err);
     } else {
-      console.log('\nuploaded %s to %s'
-        ,  files.image.filename
-        , files.image.path);
+      console.log('\nuploaded %s to %s', files.image.filename, files.image.path);
+	  
+	  var safeName = files.image.filename.replace(/ /g, "-").replace(/[?\[\]/\\=<>:;,''""&$#*()|~`!{}]/g, '');
+	  var safePath = new Date().getTime() + '/' + safeName;
+	  kclient.putFile(files.image.path, safePath, function(err, response){
+		console.log("response from knox is " + response);
+		console.log("url is " + config.s3.domain + "/" + safePath);
+		
+		urlshort.shorten(config.s3.domain + "/" + safePath, function(err, response) {
+		  if (err) throw err;
+		  console.log("response from bitly is " + response.status_code);
+		console.log("response text from bitly is " + response.status_txt);
+		
+		  // See http://code.google.com/p/bitly-api/wiki/ApiDocumentation for format of returned object
+		  var shortUrl = response.data.url
+		  console.log("shortened URL is " + shortUrl);
+		  // Do something with data
+		});		
+	  });	
+		
+		
       res.redirect('/index');
     }
   });
