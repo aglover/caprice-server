13886f054c4ababd73823443984f701b41230ece
Andrew Glover <ajglover@gmail.com>
2011-12-19 15:39:11 -0500

    fixed port

--- web.js
+++ web.js
@@ -43,6 +43,6 @@ app.post('/', function(req, res, next){
 
 var port = process.env.PORT || 3000;
 
-app.listen(8080, function(){
+app.listen(port, function(){
 	console.log("listening on port " + port);
 });
